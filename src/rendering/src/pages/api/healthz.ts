ea4e624ac71d77c991377d0a7e4cfc0b6afbb779
Andy Cohen <iamandycohen@gmail.com>
2022-07-06 03:11:43 -0500

    Update healthz.ts

--- src/rendering/src/pages/api/healthz.ts
+++ src/rendering/src/pages/api/healthz.ts
@@ -3,7 +3,6 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 const robotsApi = async (_req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   // Ensure response is text/html
 
-
   return res.status(200).send("OK");
 };
 
